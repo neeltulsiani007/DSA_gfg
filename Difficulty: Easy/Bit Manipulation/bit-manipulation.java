3f58732a1aa52ae1c4c981dda6791d9bc6a47511
Neel <128151091+neeltulsiani007@users.noreply.github.com>
2024-06-21 18:59:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bit Manipulation/bit-manipulation.java
@@ -0,0 +1,34 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int i = sc.nextInt();
+
+            Solution obj = new Solution();
+            obj.bitManipulation(n, i);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static void bitManipulation(int num, int i) {
+         int s = 1<<(i-1);
+        int c = ~(1<<(i-1));
+        System.out.print((num>>(i-1) & 1) +" "+(s|num)+" "+(c&num));
+    }
+}
